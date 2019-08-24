4b94630b09b7966b9bd30069005c0655a1965112
Michael Meffie <mmeffie@sinenomine.net>
2019-08-24 15:48:19 -0400

    read from sqlite file

--- read_dataset_from_sql.py
+++ read_dataset_from_sql.py
@@ -1,16 +1,23 @@
 from sqlalchemy import create_engine
 import random as rand
 import pandas as pd
+from pprint import pprint
 
-engine = create_engine('sitelite:///', echo=False)
+engine = create_engine('sqlite:///file.db', echo=False)
 
 names = ['Ringo', 'John', 'Paul', 'George']
-report = {}
 
+# Generate some example data.
+report = {}
 for n in names:
     grades = []
     for i in range(5):
         grades.append(rand.choice(range(50,100)))
     report[n] = grades
+pprint(report)
 
-report
+df = pd.DataFrame(report)
+df.to_sql('grades', con=engine, if_exists='replace')
+
+result = engine.execute('Select * from grades').fetchall()
+pprint(result)
